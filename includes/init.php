dbb95e4a52ad4d453d735e844ba7fe8e5b909531
Jawad Yaseen <jawadyaseen6160@gmail.com>
2026-06-30 03:58:05 +0500

    Fix PHP 8 compatibility error

--- includes/init.php
+++ includes/init.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 if (session_status() === PHP_SESSION_NONE) session_start();
 
-define('ROOT_PATH', dirname(__DIR__, 0) . '/');  // adjust depth as needed
+define('ROOT_PATH', dirname(__DIR__) . '/');  // adjust depth as needed
 define('ROOT_URL', '/');  // Change to '/ohrs/' if placed in a subdirectory
 
 require_once __DIR__ . '/db.php';
